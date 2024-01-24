1c5a770c8e7fe1c075f11fcf5230dacb0d6d0a52
Wangyuan Zhang <wyzhang@google.com>
2024-01-23 23:13:35 -0800

    update: print loaded checkpoint state_dict

--- llama/generation.py
+++ llama/generation.py
@@ -104,6 +104,7 @@ class Llama:
             checkpoints
         ), f"Loading a checkpoint for MP={len(checkpoints)} but world size is {model_parallel_size}"
         ckpt_path = checkpoints[get_model_parallel_rank()]
+        print(f'wyzhang: loading ckpt path {ckpt_path}')
         checkpoint = torch.load(ckpt_path, map_location="cpu")
         with open(Path(ckpt_dir) / "params.json", "r") as f:
             params = json.loads(f.read())
@@ -116,10 +117,13 @@ class Llama:
         tokenizer = Tokenizer(model_path=tokenizer_path)
         model_args.vocab_size = tokenizer.n_words
         torch.set_default_tensor_type(torch.cuda.HalfTensor)
+        print(f'wyzhang: create model')
         model = Transformer(model_args)
         model.load_state_dict(checkpoint, strict=False)
+        state_dict = model.state_dict()
+        for weight in state_dict:
+            print(f'{weight:40} {state_dict[weight].size()}')
         print(f"Loaded in {time.time() - start_time:.2f} seconds")
-
         return Llama(model, tokenizer)
 
     def __init__(self, model: Transformer, tokenizer: Tokenizer):
